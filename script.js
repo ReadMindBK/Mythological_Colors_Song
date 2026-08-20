38cf0843bfc4042ead9604df6faa9b602740c7e7
READMIND <readmindbook@gmail.com>
2026-08-20 19:01:07 -0300

    script.js

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ function f(s){if(!isFinite(s))return"00:00";let m=Math.floor(s/60),x=Math.floor(
 p.onclick=()=>{if(a.paused){a.play();p.textContent='⏸ Pausa';p.animate([{transform:'scale(1)'},{transform:'scale(1.08)'},{transform:'scale(1)'}],{duration:250});}else{a.pause();p.textContent='▶ Escuchar';}}
 a.ontimeupdate=()=>{r.value=(a.currentTime/(a.duration||1))*100;t.textContent=f(a.currentTime)+' / '+f(a.duration)}
 r.oninput=()=>a.currentTime=(r.value/100)*(a.duration||0);
-a.onended=()=>p.textContent='▶ Escuchar';
+a.onended=()=>p.textContent='▶ Hear';
